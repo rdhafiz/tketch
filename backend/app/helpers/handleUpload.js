ac46d17726b7788bb776cc5abec3867a961a02a5
Khalid Imran <ekhalid090@gmail.com>
2024-02-13 16:44:50 +0600

    fixed hamdle upload image

--- backend/app/helpers/handleUpload.js
+++ backend/app/helpers/handleUpload.js
@@ -1,10 +1,15 @@
 const path = require("path")
+const fs = require('fs');
 
 const handleUpload = {
     uploadFile: async (file)  => {
         const fileExt = path.extname(file.name)
         const fileNewName = file.name.replace(fileExt, '').toLowerCase().split(' ').join('-') + '-' + Date.now();
         const fileName = fileNewName + fileExt;
+        // Ensure the upload directory exists
+        if (!fs.existsSync('./uploads/')) {
+            fs.mkdirSync('./uploads/', { recursive: true });
+        }
         const uploadPath = './uploads/' + fileName;
         await file.mv(uploadPath)
         return fileName
